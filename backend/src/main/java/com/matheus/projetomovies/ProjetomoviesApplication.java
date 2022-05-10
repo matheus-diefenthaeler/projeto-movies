cb899c9848e5c39f2643c7e3a72b41c418c08c45
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-05-10 20:03:32 -0300

    Project Created

---
+++ backend/src/main/java/com/matheus/projetomovies/ProjetomoviesApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.projetomovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheus/projetomovies/ProjetomoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.projetomovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
