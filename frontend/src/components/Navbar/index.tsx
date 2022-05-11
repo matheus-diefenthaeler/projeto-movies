6bff68d50940d1eadc0c4079fcbc296c9e374cf7
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-05-10 22:22:42 -0300

    Updating Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,14 +1,15 @@
 import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./style.css";
 
 function Navbar() {
   return (
     <header>
-      <nav>
-        <div>
+      <nav className="container">
+        <div className="projectmovie-nav-content">
           <h1>Movie's Project</h1>
           <a href="https://github.com/matheus-diefenthaeler">
-            <div>
-              <GithubIcon />
+            <div className="projectmovie-contact-conteiner">
+              <GithubIcon />                
               <p>/Matheus Diefenthaeler</p>
             </div>
           </a>
