85a27640af4a295be416a4608854469e2af70013
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-05-11 20:19:01 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,25 +1,33 @@
 import MovieScore from "components/MovieScore";
+import { Link } from "react-router-dom";
 
 function MovieCard() {
-    
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-    
-    return(
-        <div>
-    <img className="projectmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="projectmovie-card-bottom-container">
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
+  return (
+    <div>
+      <img
+        className="projectmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="projectmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
-        <div className="btn btn-primary projectmovie-btn">Avaliar</div>
+
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary projectmovie-btn">Avaliar</div>
+        </Link>
+      </div>
     </div>
-</div>
-    );
+  );
 }
 
 export default MovieCard;
