d48474220a3ccd63ea6b9898c67d6deb9a70dc31
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-05-13 21:15:47 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/matheus/projetomovies/entities/Movie.java
@@ -0,0 +1,30 @@
+package com.matheus.projetomovies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+
+}
---
+++ backend/src/main/java/com/matheus/projetomovies/entities/Score.java
@@ -0,0 +1,31 @@
+package com.matheus.projetomovies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+}
